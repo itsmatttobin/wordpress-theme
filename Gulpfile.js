5e96f837a06b16cf71bec412a278eae991ccc0e6
Matt Tobin <matt@matttobin.co.uk>
2017-09-17 17:09:28 +0100

    Update gulp config

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,41 +3,47 @@ var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var babel = require('gulp-babel');
 
 // SASS
 gulp.task('sass', function() {
-    return gulp.src('assets/scss/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('assets/css/src'));
+  return gulp.src('assets/scss/style.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('assets/css/src'));
 });
 
 // Minify CSS
 gulp.task('minify-css', function() {
-  return gulp.src('assets/css/src/style.css')
+  return gulp.src('assets/css/src/**/*.css')
     .pipe(cleanCSS())
-    .pipe(rename('style.min.css'))
+    .pipe(rename(function(path) {
+        path.basename += '.min';
+    }))
     .pipe(gulp.dest('assets/css/dist'));
 });
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('assets/js/src/**/*.js')
-        .pipe(uglify().on('error', swallowError))
-        .pipe(rename(function(path) {
-            path.basename += '.min';
-        }))
-        .pipe(gulp.dest('assets/js/dist'));
+  return gulp.src('assets/js/src/**/*.js')
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(uglify().on('error', swallowError))
+    .pipe(rename(function(path) {
+        path.basename += '.min';
+    }))
+    .pipe(gulp.dest('assets/js/dist'));
 });
 
 // Stop watch tasks from stopping on error
 function swallowError (error) {
-    console.log(error.toString());
-    this.emit('end');
+  console.log(error.toString());
+  this.emit('end');
 }
 
 // Default task
 gulp.task('default', function() {
-    gulp.watch('assets/scss/**/*.scss', ['sass']);
-    gulp.watch('assets/css/src/style.css', ['minify-css']);
-    gulp.watch('assets/js/src/**/*.js', ['minify-js']);
+  gulp.watch('assets/scss/**/*.scss', ['sass']);
+  gulp.watch('assets/css/src/**/*.css', ['minify-css']);
+  gulp.watch('assets/js/src/**/*.js', ['minify-js']);
 });
